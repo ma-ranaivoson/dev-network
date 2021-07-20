26b8624b60d6180954cf4470b340bd2b8ceb2984
ma_rvs <rv.nirmalala@gmail.com>
2021-07-20 19:19:56 +0100

    Implementing Profile

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import EditProfile from './components/profile-forms/Edit-profile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+
 
 // Redux
 import { Provider } from 'react-redux';
@@ -47,6 +49,7 @@ const App = () => {
                             />
                             <Route exact path='/login' component={Login} />
                             <Route exact path='/profiles' component={Profiles} />
+                            <Route exact path='/profile/:id' component={Profile} />
                             <PrivateRoute exact path='/dashboard' component={Dashboard} />
                             <PrivateRoute exact path='/create-profile' component={CreateProfile} />
                             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
