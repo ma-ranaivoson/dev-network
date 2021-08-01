1105cd403a59bf05cd9f49b9643622b71abfd8ed
ma_rvs <rv.nirmalala@gmail.com>
2021-08-01 17:14:28 +0100

    Adding like and unlike functionality

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -15,3 +15,6 @@ export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const UPDATE_EDUCATION = 'UPDATE_EDUCATION';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 export const GET_REPOS = 'GET_REPOS';
+export const GET_POSTS = 'GET_POSTS';
+export const POST_ERROR = 'POST_ERROR';
+export const UPDATE_LIKES = 'UPDATE_LIKES';
