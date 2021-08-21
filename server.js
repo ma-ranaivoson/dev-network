08cd58ec0464d9b783f07fd269bc6b22359bad96
ma_rvs <rv.nirmalala@gmail.com>
2021-08-21 18:32:46 +0100

    Adding production.json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,13 +10,21 @@ app.use(express.json({ extended: false }));
 // Connect database
 connectDB();
 
-app.get('/', (req, res) => res.send('API running'));
-
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static Server
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 const PORT = process.env.PORT || 5000;
 
